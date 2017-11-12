f1e45716182086d9d59febfd788e4922a66b4c89
Monal Shadi <monalshadi16@gmail.com>
2017-11-12 21:29:09 +0530

    Fixed Indentations, follow java conventions

--- Process.java
+++ Process.java
@@ -1,16 +1,15 @@
-class Process
-{
-    String pid;   // Process ID
-    int aT,bT,p;  // arrival, burst, priority 
-    double iI,iP; // I/O interrupt, I/O processing(waiting)
-    
-    public Process(String[] metadata, int rAT)
-    {   
-        this.pid = metadata[0];
-        this.aT = rAT+Integer.parseInt(metadata[1]);	//calculating actual arrival time from relative arrival time
-        this.bT = Integer.parseInt(metadata[2]);
-        this.iI = Double.parseDouble(metadata[3]);
-        this.iP = Double.parseDouble(metadata[4]);
-        this.p = Integer.parseInt(metadata[5]);
-    }
+class Process {
+	private String pid;   // Process ID
+	private int aT,bT,p;  // arrival, burst, priority 
+	private double iI,iP; // I/O interrupt, I/O processing(waiting)
+
+	public Process(String[] metaData, int relativeArrivalTime) {   
+		this.pid = metaData[0];
+			// Calculating actual arrival time from relative arrival time
+		this.aT = relativeArrivalTime + Integer.parseInt(metaData[1]);
+		this.bT = Integer.parseInt(metaData[2]);
+		this.iI = Double.parseDouble(metaData[3]);
+		this.iP = Double.parseDouble(metaData[4]);
+		this.p = Integer.parseInt(metaData[5]);
+	}
 }
