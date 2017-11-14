d40047a01bf89d4d1797fe832a657101995241c5
Monal5031 <201652013@iiitvadodara.ac.in>
2017-11-14 13:25:35 -0800

    Updated script file

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 source ../lab-venv/bin/activate
 java -jar "/home/fsociety/Desktop/Programacion/dist/Programacion.jar"
+rm log.txt
+echo ""
 echo "Plotting Graph Please Wait..."
 python3 plotter.py
 deactivate
